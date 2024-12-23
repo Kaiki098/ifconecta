fb838ec263874f1809c6323ad882bd74c152c7d5
kaiki <kaikialvarengasouza098@gmail.com>
2024-12-23 17:12:39 -0300

    remove next default code and add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+export default function Login() {
+    return (
+        <div className={styles.page}>
+            <div className={styles.card}>
+                <h1>Login</h1>
+            </div>
+        </div>
+    )
+}
