714b9fc7f9e34ddf831b9bc0efff59055f764329
kaiki <kaikialvarengasouza098@gmail.com>
2024-12-26 11:55:14 -0300

    enhance eslint configuration with jsx-a11y plugin and prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,13 +11,43 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended",
+  ),
   {
+    plugins: {
+      "jsx-a11y": jsxA11y,
+    },
     rules: {
-      "semi": ["error", "always"],
-      "quotes": ["warn", "double"]
-    }
-  }
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: false,
+          trailingComma: "all",
+          arrowParens: "always",
+          semi: true,
+          endOfLine: "auto",
+        },
+      ],
+      "jsx-a11y/alt-text": [
+        "warn",
+        {
+          elements: ["img"],
+          img: ["Image"],
+        },
+      ],
+      // "jsx-a11y/aria-props": "warn",
+      // "jsx-a11y/aria-proptypes": "warn",
+      // "jsx-a11y/aria-unsupported-elements": "warn",
+      // "jsx-a11y/role-has-required-aria-props": "warn",
+      // "jsx-a11y/role-supports-aria-props": "warn",
+      // "react/no-unknown-property": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
